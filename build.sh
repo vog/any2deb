903b5e085814e4e6b8e66b214e101cd9d8e392d9
vog <vog@7edfa1aa-b5ec-0310-85b9-a344310dd3f3>
2006-03-07 15:10:03 +0000

    any2deb build code is now part of the any2deb usage examples :-)
    git-svn-id: file:///home/vog/x/njh/any2deb/trunk@40 7edfa1aa-b5ec-0310-85b9-a344310dd3f3

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
+# ensure any2deb is executable
 chmod 755 any2deb
 
-VERSION=`./any2deb | grep "any2deb" | head -1 | cut -d- -f2`
-VERSION=`echo $VERSION`
+# retrieve build command, it's an example of any2deb ;-)
+BUILD_CMD=`./any2deb | grep "any2deb.tar"`
 
-tar -cv -f any2deb.tar any2deb
-./any2deb \
-    any2deb \
-    "$VERSION" \
-    any2deb.tar \
-    /usr/bin \
-    "--to-deb --to-rpm"
+# build
+tar -c -f any2deb.tar any2deb
+echo running: $BUILD_CMD
+eval $BUILD_CMD
 rm -f any2deb.tar
