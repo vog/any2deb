5ce25ded82f940778a7ec83627097c670ce007ee
vog <vog@7edfa1aa-b5ec-0310-85b9-a344310dd3f3>
2006-03-07 14:55:42 +0000

    import project any2deb
    git-svn-id: file:///home/vog/x/njh/any2deb/trunk@38 7edfa1aa-b5ec-0310-85b9-a344310dd3f3

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+chmod 755 any2deb
+
+VERSION=`./any2deb | grep "any2deb" | head -1 | cut -d- -f2`
+VERSION=`echo $VERSION`
+
+tar -cfv any2deb.tar any2deb
+./any2deb \
+    any2deb \
+    "$VERSION" \
+    any2deb.tar \
+    /usr/bin \
+    "--to-deb --to-rpm"
+rm -f any2deb.tar
