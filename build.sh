4b8be3d5e8e7a39798e808f0a62f0aa1a28efa12
vog <vog@7edfa1aa-b5ec-0310-85b9-a344310dd3f3>
2006-03-07 23:50:52 +0000

    several bugfixes
    git-svn-id: file:///home/vog/x/njh/any2deb/trunk@43 7edfa1aa-b5ec-0310-85b9-a344310dd3f3

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 chmod 755 any2deb
 
 # retrieve build command, it's an example of any2deb ;-)
-BUILD_CMD=`./any2deb | grep "any2deb.tar"`
+BUILD_CMD=`./any2deb | grep -C1 any2deb.tar`
 
 # build
 tar -c -f any2deb.tar any2deb
-echo running: $BUILD_CMD
-eval $BUILD_CMD
+echo running:
+echo "$BUILD_CMD"
+echo
+eval "$BUILD_CMD"
 rm -f any2deb.tar
